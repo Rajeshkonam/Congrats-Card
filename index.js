c498c6db0a125368d1ca3d4dcaa12e13ce724afd
rajesh <rajeshkonam701@gamail.com>
2023-09-09 06:33:11 +0000

    commit message

---
+++ index.js
@@ -0,0 +1,21 @@
+const element = (
+  <div className="div1">
+    <h1>Congratulations</h1>
+    <div className="div2">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"
+        className="img1"
+      />
+      <p className="p1">Kiran v</p>
+      <p className="p2">
+        Vishnu Institute of Computer Education and Technology, Bhimavaram.
+      </p>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
+        className="img2"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
